f9dc1d7920e7e912babf6d1d0f329dfa60186193
pujith2007 <86406123+pujith2007@users.noreply.github.com>
2021-06-24 16:43:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+//variable declaration
+var app;
+app=creatSprite(x,y,width,height);
+var pujith=10;
+var arr1=[1,"name",pujith];
+console.log(pujith);
+console.log(arr1);
+var bool=true;
+console.log(bool);
+
+
+//function
+function add();
+function add(){
+  console.log("addition");
+}
+if(pujith>1){
+  console.log("pujith>1");
+}
+else{
+  console.log("pujith>100");
+}
+for(var i=0;i<100;i=i+10){
+  Text("forloop",i,50);
+}
+//physics engine
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+//constraint - movement of one body will effect the other body
+
+//class declaration
+class srivasthav{ 
+  constructor(){
+
+  }
+  display(){
+
+  }
+}
+class pujith2 extends srivasthav{
+
+}
+//database 
+//ref-ref data base
+//update-set the value
+//read-get the value 
